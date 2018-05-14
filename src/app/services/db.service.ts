7929f750481be69664400175ccbb219d2b676b13
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-05-14 11:00:33 +0530

    feat: added nedb for client side storage

---
+++ src/app/services/db.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import * as Datastore from 'nedb';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DBService {
+  db;
+  constructor() {
+    this.db = new Datastore({ filename: 'ink/db' });
+    this.db.loadDatabase(e => {});
+    const doc = {
+      hello: 'Hambyui'
+    };
+    this.db.insert(doc, function(err, newDoc) {
+      console.log('database insert done!', doc);
+    });
+    this.db.find({}, function(err, docs) {
+      console.log('database fond done!', docs);
+    });
+  }
+}
