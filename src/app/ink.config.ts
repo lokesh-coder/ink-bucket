4294513ed59c3cd7727c2c883d595bec41c2e977
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-06-07 21:32:32 +0530

    feat: add empty state for buckets

--- src/app/ink.config.ts
+++ src/app/ink.config.ts
@@ -10,5 +10,7 @@ export const DB_REMOTE_SYNC_URL = `http://${window.location.hostname}:10101/`;
 export const BOARD_DEFAULT_NAME = 'Beautiful';
 export const BOARD_DEFAULT_DESC = 'Default Board';
 
+export const DEFAULT_BUCKET_NAME = `Bucket #`;
+
 export const GITHUB_ACCESS_TOKEN_NAME = 'inkapp_access_token';
 export const GIST_NAME = 'INKAPP :: SYNC SETTINGS';
