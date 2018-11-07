38b534c4c61f3fd050be4cd0ad8c1af8870c3fa1
Lokesh <l.rajendran@devon.nl>
2018-11-07 16:36:48 +0530

    refactor: clean up

--- src/app/library/elements/index.ts
+++ src/app/library/elements/index.ts
@@ -1,5 +1,4 @@
 export { ActionItemElement } from './action-item/action-item.element';
 export { BillboardElement } from './billboard/billboard.element';
 export { EditableTitleElement } from './editable-title/editable-title.element';
-export { UserCardElement } from './user-card/user-card.element';
 
